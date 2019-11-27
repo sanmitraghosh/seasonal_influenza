01866657434c9583b08577917ecc40e9760a6a7b
Joshua Blake <joshuab@mrc-bsu.cam.ac.uk>
2019-11-27 16:08:14 +0000

    Refactor entry file to allow reuse

--- test_inference_icu.py
+++ test_inference_icu.py
@@ -21,47 +21,55 @@ from util.diagnostics import effective_sample_size
 Transform = True
 Hospitalisation = False
 iterations = 100000
-comp_iterations = 10000
 burnin = 40000
 thin = 30
-try:
-    param_filename = sys.argv[1]
-except IndexError:
-    param_filename = './results/icu_amgs.p'
-print(param_filename)
 
-Data = np.loadtxt('data/2017_18_with_pop.csv', dtype=int, delimiter=',')
+def get_output_filename():
+    try:
+        param_filename = sys.argv[1]
+    except IndexError:
+        param_filename = './results/icu_amgs.p'
+    return param_filename
 
-logP = LogPosterior(Data[:,0], Transform, Hospitalisation, catchment_pop=Data[:,1])
-start = [0.16, 0.56, 0.0000081, -0.001, 0.00078, 10]
-X0 = np.hstack(start)
-Init_scale = np.array([0.05,0.03,0.000000051, 0.03, 0.0000015,0.5, 0.0000015, 0.5])
+def read_data(in_file):
+    return np.loadtxt(in_file, dtype=int, delimiter=',')
 
-# Now run the AMGS sampler
-start_amgs = time.time()
-if Transform:
-        X0 = logP._transform_from_constraint(X0)
-        Init_scale = 1*np.abs(X0)
-        cov = np.diag(Init_scale)
-        sampler = AdaptiveMetropolis(logP, mean_est=X0, cov_est=cov, tune_interval = 1)
-        MCMCsampler = MCMC(sampler, logP, X0, iterations)
-        trace = MCMCsampler.run()[0]
-else:
-        sampler = AdaptiveMetropolis(logP, mean_est=X0, cov_est=None, tune_interval = 1)
-        MCMCsampler = MCMC(sampler, logP, X0, iterations) 
-        trace = MCMCsampler.run()       
+def run_MCMC(data):
+    logP = LogPosterior(data[:,0], Transform, Hospitalisation, catchment_pop=data[:,1])
+    start = [0.16, 0.56, 0.0000081, -0.001, 0.00078, 10]
+    X0 = np.hstack(start)
+    Init_scale = np.array([0.05,0.03,0.000000051, 0.03, 0.0000015,0.5, 0.0000015, 0.5])
 
-
-end_amgs = time.time()
-
-pickle.dump(trace, open(param_filename, 'wb'))
-
+    # Now run the AMGS sampler
+    if Transform:
+            X0 = logP._transform_from_constraint(X0)
+            Init_scale = 1*np.abs(X0)
+            cov = np.diag(Init_scale)
+            sampler = AdaptiveMetropolis(logP, mean_est=X0, cov_est=cov, tune_interval = 1)
+            MCMCsampler = MCMC(sampler, logP, X0, iterations)
+            trace = MCMCsampler.run()[0]
+    else:
+            sampler = AdaptiveMetropolis(logP, mean_est=X0, cov_est=None, tune_interval = 1)
+            MCMCsampler = MCMC(sampler, logP, X0, iterations) 
+            trace = MCMCsampler.run()       
+    return trace
 
 
-trace_post_burn = trace[burnin:,:]
-py_thinned_trace_amgs = trace_post_burn[::thin,:]
-amgs_ess = effective_sample_size(py_thinned_trace_amgs)
+def write_trace(trace, filename):
+    pickle.dump(trace, open(filename, 'wb'))
 
 
-print('ESS AMGS: ', amgs_ess)
-print('AMGS time taken: ', end_amgs - start_amgs)
+def thin_trace(trace):
+    trace_post_burn = trace[burnin:,:]
+    py_thinned_trace_amgs = trace_post_burn[::thin,:]
+    return py_thinned_trace_amgs
+
+
+if __name__ == '__main__':
+    in_file = 'data/2017_18_with_pop.csv'
+    data = read_data(in_file)
+    trace = run_MCMC(data)
+    write_trace(trace, get_output_filename())
+    thinned_trace = thin_trace(trace)
+    ess = np.mean(effective_sample_size(thinned_trace))
+    print('ESS AMGS: ', ess)
