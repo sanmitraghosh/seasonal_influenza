3e5f43a65adfed3f6eb2e65b77bf4e14e98393d3
Joshua Blake <joshuab@mrc-bsu.cam.ac.uk>
2019-11-29 11:35:04 +0000

    Base forecasts on the whole period

--- forecast.py
+++ forecast.py
@@ -4,7 +4,10 @@ from test_inference_icu import read_data, run_MCMC, write_trace, thin_trace
 if __name__ == '__main__':
     in_file = 'data/2017_18_with_pop.csv'
     data = read_data(in_file)
-    for i in range(10, len(data)+1, 2):
+    for i in range(2, 10, 2):
+        print('For week {}'.format(i))
+        data_to_use = data[0:i]
+        assert len(data_to_use) == i
         out_file = 'results/forecast/week{}.p'.format(i)
         trace = run_MCMC(data[0:i])
         write_trace(trace, out_file)
