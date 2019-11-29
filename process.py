03ba2a38c66fa63a0dbaba7af9913924670446e2
Joshua Blake <joshuab@mrc-bsu.cam.ac.uk>
2019-11-29 14:51:37 +0000

    A quick script to predict across many weeks

---
+++ process.py
@@ -0,0 +1,5 @@
+import subprocess
+COMMAND = "python display.py -r results/forecast/week{}.p -o results/forecast/simulated/week{}.png predict -b4000 -t1"
+for i in range(2, 10, 2):
+    print(COMMAND.format(i,i))
+    subprocess.run(COMMAND.format(i, i), shell=True)
